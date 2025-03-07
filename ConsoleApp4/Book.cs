edd986e33c0de663de1137182062313d99b0d322
Petrik Diak <diak@petrik.hu>
2025-03-07 12:24:18 +0100

    Konstruktor és getter kész

--- ConsoleApp4/Book.cs
+++ ConsoleApp4/Book.cs
@@ -8,5 +8,23 @@ namespace ConsoleApp4
 {
 	internal class Book
 	{
+		string title;
+		string author;
+		int pageCount;
+		int releaseYear;
+
+		public Book(string title, string author, int pageCount, int releaseYear)
+		{
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.releaseYear = releaseYear;
+		}
+
+		public string Title { get => title; set => title = value; }
+		public string Author { get => author; set => author = value; }
+		public int PageCount { get => pageCount; set => pageCount = value; }
+		public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+
 	}
 }
